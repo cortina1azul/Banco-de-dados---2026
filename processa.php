ece15f25208fd461b2bbe1f97b5c4919feb8b867
Otávio Martins <c0rt1n4.azul@gmail.com>
2026-05-05 08:44:30 -0300

    Add files via upload

---
+++ processa.php
@@ -0,0 +1,36 @@
+<?php
+
+$host = 'localhost';
+$bdname = 'projeto_site';
+$usuario = 'root';
+$senha = '';
+
+try {
+    $conexao = new PDO("mysql:host=$host;dbname=$bdname;charset=utf8", $usuario, $senha);
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro ao conectar com banco de dados: " . $e->getMessage());
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome_recebido = $_POST['nome'];
+    $email_recebido = $_POST['email'];
+    $mensagem_recebida = $_POST['mensagem'];
+
+    $sql = "INSERT INTO contatos (nome, email, mensagem) VALUES (:nome, :email, :mensagem)";
+    $stmt = $conexao->prepare($sql);
+
+    $stmt->bindParam(':nome', $nome_recebido);
+    $stmt->bindParam(':email', $email_recebido);
+    $stmt->bindParam(':mensagem', $mensagem_recebida);
+
+    if ($stmt->execute()) {
+        echo "<h1>Sucesso!</h1>";
+        echo "<p>Dados salvos no banco de dados.</p>";
+        echo "<a href='index.html'>Voltar</a>";
+    } else {
+        echo "ERRO ao tentar salvar os dados.";
+    }
+}
+
+?>
